3e9595dd32c09795a6ff87f09186f912b1cfc460
Sam Helms <sam.helms@sial.com>
2017-07-16 13:34:57 -0500

    updated speller

--- src/spell_corrector/enchant/EnchantSpeller.py
+++ src/spell_corrector/enchant/EnchantSpeller.py
@@ -1,70 +1,102 @@
 import sys
-from enchant.pypwl import PyPWL
+from enchant import request_pwl_dict
 import os
+import csv
+import itertools
 
 class EnchantSpeller:
     def __init__(self, word_list = None, 
-                pwl_loc = "/src/spell_corrector/enchant/pwl",
+                pwl_loc = "/data/pwl",
                 ):
         self.dict = None
         filepath = os.getcwd()+pwl_loc
 
+        print filepath
+
         # load the dictionary for spell correction
         try: 
-            self.dict = PyPWL(filepath)
             print("loading pwl")
+            self.dict = request_pwl_dict(filepath)
+            if not self.dict.check("sigmaaldrich"):
+                print("going to build")
+                raise IOError
+            print("loaded pwl")
         except IOError:
             print("building pwl")
             f = open(filepath,"w+")
             f.write("SAMUELHELMSISAWESOME"+ "\n")
             f.close()
-            self.dict = PyPWL(filepath)
-            self.load_words(self.dict, word_list)
-            # self.get_error_rate(self.dict, word_list)
+            self.load_words(word_list, filepath)
+            self.dict = request_pwl_dict(filepath)
 
     # NOTE: this is implemented on the correct, incorrect pairs dataset in data. Expects the proper spelling to follow a pipe
     # EX: 5 2gether | together
-    def load_words(self, d, word_list):
+    def load_words(self, word_list, filepath):
         with open(word_list) as f:
-            for line in f:
-                correctly_spelled_word = line.split("|")[1].strip().lower()
-                d.add(correctly_spelled_word)
-                
+            out_file = open(filepath, "w+")
+            for i, line in enumerate(f):
+                correctly_spelled_word = line.split("|")[0].strip().lower()
+                print>>out_file, correctly_spelled_word
+                if i % 100000 == 0:
+                    print "loaded %d words" % i
+
     # checks to see if the mispellings get corrected
-    def get_error_rate(self, d, word_list, verbose = False):
+    # correct input for eval file: csv: line: CORRECT, INCORRECT
+    def get_error_rate(self, word_list, verbose = False):
+        wf = None
+        if verbose == True:
+            wf = open("spell_comparison.txt", "w")
         approx_correct = 0.0
         all_correct = 0.0
         total = 0.0
         no_correct_list = []
         approx_correct_list = []
         with open(word_list) as f:
-            for line in f:
-                incorrectly_spelled_word = line.split("|")[0].split("\t")[1].strip().lower()
-                correctly_spelled_word = line.split("|")[1].strip().lower()
-                suggestions = d.suggest(incorrectly_spelled_word)
+            csvreader = csv.reader(f, delimiter=',', quotechar='"')
+            # skip header
+            csvreader.next()
+            for line in csvreader:
+                incorrectly_spelled_words = line[1].strip().lower()
+                correctly_spelled_word = line[0].strip().lower()
+
+                suggestions_matrix = []
+                for w in incorrectly_spelled_words.split(" "):
+                    if not self.dict.check(w):
+                        suggestions_matrix.append(self.dict.suggest(w))
+
+                suggestions = list(itertools.product(*suggestions_matrix))
+
+                suggestion_list = [" ".join(el) for el in suggestions]
+
+                # print suggestion_list
        
-                if len(suggestions) > 0 and suggestions[0] == correctly_spelled_word:
+                if len(list(suggestions)) > 0 and " ".join(suggestions[0]) == correctly_spelled_word:
                     all_correct+=1
-                elif len(suggestions) > 0 and correctly_spelled_word in suggestions:
+                elif len(list(suggestions)) > 0 and correctly_spelled_word in suggestion_list:
                     approx_correct += 1
-                    approx_correct_list.append( (incorrectly_spelled_word, correctly_spelled_word, suggestions) )
+                    approx_correct_list.append( (incorrectly_spelled_words, correctly_spelled_word, suggestions) )
                 else:
-                    no_correct_list.append( (incorrectly_spelled_word, correctly_spelled_word, suggestions) )
+                    no_correct_list.append( (incorrectly_spelled_words, correctly_spelled_word, suggestions) )
 
                 total+=1
+
+                if verbose == True:
+                    print "term: " +incorrectly_spelled_words," || current suggestion: "+correctly_spelled_word+" || our suggests: "+" ".join(suggestion_list)
+
+                    print>>wf, "term: " +incorrectly_spelled_words," || current suggestion: "+correctly_spelled_word+" || our suggests: "+" ".join(suggestion_list)
 
                 if total % 10 == 0:
                     print("results:")
                     print "wholly correct rate: %f, somewhat correct rate: %f, total rate: %f" % (all_correct/total, approx_correct/total, (all_correct+approx_correct)/total)
-                    if verbose == True:
-                        print("somewhat correct suggestion terms: ")
-                        print(approx_correct_list)
-                        print("no correct suggestion terms: ")
-                        print(no_correct_list)
+
+                        # print("somewhat correct suggestion terms: ")
+                        # print(approx_correct_list)
+                        # print("no correct suggestion terms: ")
+                        # print(no_correct_list)
 
     def get_suggestions(self, word):
         suggestions = self.dict.suggest(word)
-        return suggestions[0:3] # come up with a more advanced method for selecting which of the suggestion to return -- edit distance based?
+        return suggestions # come up with a more advanced method for selecting which of the suggestion to return -- edit distance based?
 
     def check(self, word):
         return self.dict.check(word)
@@ -76,7 +108,10 @@ if __name__ == "__main__":
     es = EnchantSpeller(sys.argv[1])
 
     print es.get_suggestions("0n")
-
-    # es2 = EnchantSpeller()
+    print es.check("sigmaaldrich")
+    print es.get_suggestions("isooctyltrimethoxysilane")
+    print es.get_suggestions("isooctyltrimethoxysilane")
+    print es.get_suggestions("isooctyltrimethoxysilane")
+    print es.get_suggestions("isooctyltrimethoxysilane")
 
     
